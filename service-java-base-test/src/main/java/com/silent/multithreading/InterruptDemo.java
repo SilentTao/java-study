9abed694206a7b9644f902b381376858408c4029
silent <silentiu@smartmovt.com>
2019-03-19 08:31:51 +0800

    提交interrupt,wait,sleep demo

---
+++ service-java-base-test/src/main/java/com/silent/multithreading/InterruptDemo.java
@@ -0,0 +1,33 @@
+package com.silent.multithreading;
+
+public class InterruptDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable interruptDemo=new Runnable() {
+            @Override
+            public void run() {
+                int i=0;
+                try{
+                    while (!Thread.currentThread().isInterrupted()){
+                        Thread.sleep(100);
+                        i++;
+                        System.out.println(Thread.currentThread().getName() +"("+Thread.currentThread().getState()+")"+"loop"+i);
+                    }
+                }catch (InterruptedException e){
+                    System.out.println(Thread.currentThread().getName() +"("+Thread.currentThread().getState()+")"+"this is InterruptedException");
+                }
+            }
+        };
+        Thread t1=new Thread(interruptDemo,"t1");
+        System.out.println(t1.getName()+" ("+t1.getState()+")" +"is new");
+        t1.start();
+        System.out.println(t1.getName()+" ("+t1.getState()+")" +"is started");
+
+        Thread.sleep(300);
+        t1.interrupt();
+        System.out.println(t1.getName()+" ("+t1.getState()+")" +"is interrupted");
+        Thread.sleep(300);
+        System.out.println(t1.getName()+" ("+t1.getState()+")" +"is interrupted  now");
+
+    }
+}
--- service-java-base-test/src/main/java/com/silent/multithreading/WaitSleepDemo.java
+++ service-java-base-test/src/main/java/com/silent/multithreading/WaitSleepDemo.java
@@ -5,64 +5,63 @@ package com.silent.multithreading;
  * 1.sleep是Thread类中的方法，是一个native方法，wait是object中的方法，
  * 2.sleep的时候是不会释放锁和cup的，wait会释放资源（cup,锁）
  * 3.sleep外部可以使用，wait只能够在synchronized中使用
- *
- *
+ * <p>
+ * <p>
  * 锁池（EntryList）和等待池(WaitList)
  * notifyAll 会让所有处于等待池总的线程去竞争锁池中的锁
  * notify   会随机让一个线程去竞争线程池中的锁
-*/
+ */
 public class WaitSleepDemo {
 
     public static void main(String[] args) {
-        final Object lock=new Object();
-       new Thread(new Runnable() {
+        //创建锁对象
+        final Object lock = new Object();
+        // 线程AAA
+      Thread aaa=  new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("thread A is waiting to get lock");
-                synchronized (lock){
-                    System.out.println("thread A get lock ");
+                System.out.println("thread AAA is waiting to get lock");
+                synchronized (lock) {
+                    System.out.println("thread AAA get lock ");
                     try {
-                        Thread.sleep(20);
-                    System.out.println("thread A do wait method");
-                    lock.wait();
-                      //  Thread.sleep(1000);
-                    System.out.println("Thread A is notify");
-                    System.out.println("thread A is done");
+
+                        System.out.println("thread AAA do wait method");
+                        Thread.sleep(40);
+                       // Thread.sleep(20);
+                        System.out.println("thread AAA is done");
+
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             }
-        }).start();
+        });
+      aaa.start();
 
         try {
-            Thread.sleep(10);
+            //主线程睡眠10毫秒等待让两个线程启动有个竞争锁的过程
+            Thread.sleep(20);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        new Thread(new Runnable() {
+        // 线程BBB
+       Thread bbb= new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("thread Bis waiting to get lock");
-                synchronized (lock){
-                    System.out.println("thread B get lock ");
+                System.out.println("thread BBB is waiting to get lock");
+                synchronized (lock) {
+                    System.out.println("thread BBB get lock ");
                     try {
-                        Thread.sleep(20);
+                       Thread.sleep(20);
+                        System.out.println("thread BBB do wait method");
+                        System.out.println("thread BBB is done");
                         lock.notifyAll();
-                        System.out.println("thread B do wait method");
-                        System.out.println("thread B is done");
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             }
-        }).start();
-        Thread thread=new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-            }
         });
-
+        bbb.start();
     }
 }
