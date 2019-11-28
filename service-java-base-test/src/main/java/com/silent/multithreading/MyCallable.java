5024efe6eb28f28466a1020d4dae98256f965e4d
liutao8 <liutao8@vipkid.com.cn>
2019-11-28 21:29:20 +0800

    提交修改

--- service-java-base-test/src/main/java/com/silent/multithreading/MyCallable.java
+++ service-java-base-test/src/main/java/com/silent/multithreading/MyCallable.java
@@ -3,6 +3,7 @@ package com.silent.multithreading;
 import java.util.concurrent.Callable;
 
 public class MyCallable implements Callable<String> {
+
     @Override
     public String call() throws Exception {
         String value="test";
---
+++ service-java-base-test/src/main/java/com/silent/socket/Client.java
@@ -0,0 +1,37 @@
+package com.silent.socket;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Author liutao
+ * Date 2019/11/28 8:56 上午
+ * Description:
+ * Version: 1.0
+ **/
+public class Client {
+
+
+    public static void main(String[] args) throws Exception {
+        Socket client = new Socket("127.0.0.1", 43211);
+        try {
+            PrintWriter output =
+                    new PrintWriter(client.getOutputStream(), true);
+            Scanner cin = new Scanner(System.in);
+            String words;
+
+            while (cin.hasNext()) {
+                words = cin.nextLine();
+
+                output.println(words);
+
+                System.out.println("aaa" + words);
+            }
+
+            cin.close();
+        } finally {
+            client.close();
+        }
+    }
+}
